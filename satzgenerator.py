b800782a7b6dba7bff560a51d9cc835ef601117f
davidak <git@davidak.de>
2013-05-26 19:46:35 +0200

    großer rewrite mit peewe als orm (datenbank) und Ajax für das voting, dadurch sicherer. stabile version von bottle und neue version von bootstrap

--- satzgenerator.py
+++ satzgenerator.py
@@ -1,228 +1,140 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from bottle import route, run, template, static_file, error, request, response, redirect, default_app
-import sqlite3
+from bottle import route, template, static_file, error, request, response, redirect, default_app, run, debug
+from peewee import *
 from pyzufall import pyzufall as z
+from datetime import datetime
 import random
-import time
+import sys
 
-#DEBUG=True
+debug(mode=True)
 
 random.seed() # Zufallsgenerator initialisieren
 
-def counter():
-	randb = int( request.cookies.get('randb', str(random.randint(7,15))) )
-	visits = int( request.cookies.get('visits', '0') )
-	visits += 1
-	if visits >= randb:
-		# Anzahl Datensätze
-		#sql = "SELECT COUNT(*) FROM satz"
-		
-		con = sqlite3.connect('datenbank.db')
-		sql = "SELECT id FROM satz WHERE pro >= contra"
-		c = con.cursor()
-		c.execute(sql)
-		pro_satz_ids = c.fetchall()
-		
-		randb_satz = random.choice(pro_satz_ids)[0] #ID von zufälligem, gutem Satz aus DB
-		
-		c.close()
-		con.close()
-		
-		response.delete_cookie('visits')
-		response.delete_cookie('randb')
-	else:
-		randb_satz = ''
-		response.set_cookie('visits', str(visits))
-	return randb_satz
+def jetzt():
+	return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+#db = MySQLDatabase('davidak_satzgenerator', user='davidak',passwd='9335be4gnjcvd7hbxp5f')
+db = SqliteDatabase('dev.db')
+
+class DBModel(Model):
+	class Meta:
+		database = db
+
+class Satz(DBModel):
+	uid = CharField(primary_key=True)
+	created = DateTimeField()
+	updated = DateTimeField(null=True)
+	pro = IntegerField(default=0, null=True)
+	kontra = IntegerField(default=0, null=True)
+	tmp = BooleanField(default=True)
+	satz = CharField()
+
+class Benutzer(DBModel):
+	satz_uid = CharField()
+	ip = CharField()
+	voted = DateTimeField()
+
+def erstelle_tabellen():
+    db.connect()
+    Satz.create_table()
+    Benutzer.create_table()
+
+#erstelle_tabellen()
+
+def neuen_satz_speichern(satz):
+	for x in range(10): # 10 Versuche, den Satz zu speichern
+		try:
+			uid = ''.join(random.choice('abcdefghiklmnopqrstuvwxyz') for x in range(5))
+			Satz.create( # Neuen Satz in DB speichern
+			uid = uid,
+			created = jetzt(),
+			satz = satz
+			)
+			return uid
+		except:
+			print('Fehler: Satz konnte nicht in die Datenbank gespeichert werden.')
+	print('Der Satz konnte auch beim 10. Versuch nicht in die Datenbank gespeichert werden. Das ist vermutlich ein Datenbank-Problem und es sollte der Administrator informiert werden.')
+
+def neuen_satz_generieren():
+	satz = z.satz()
+	try:
+		uid = Satz.select().where(Satz.satz == satz).get().uid # Wenn Satz bereits in DB, gib UID zurück
+		#print('Satz bereits in DB mit UID ' + uid + '!')
+		return uid
+	except Satz.DoesNotExist: # ansonsten speichern
+		uid = neuen_satz_speichern(satz)
+		#print('Neuen Satz generiert und mit UID ' + uid + ' in DB gespeichert.') # Neue UID
+		return uid
+
+def satz_positiv_bewerten(uid):
+	try:
+		Satz.update(updated = jetzt(), pro = Satz.pro + 1, tmp = False).where(Satz.uid == uid).execute()
+	except:
+		print('Fehler: Die Bewertung konnte nicht in der Datenbank gespeichert werden.')
+
+def satz_negativ_bewerten(uid):
+	try:
+		Satz.update(updated = jetzt(), kontra = Satz.kontra + 1, tmp = False).where(Satz.uid == uid).execute()
+	except:
+		print('Fehler: Die Bewertung konnte nicht in der Datenbank gespeichert werden.')
+
+def satz_permanent_speichern(uid):
+	try:
+		Satz.update(tmp = False).where(Satz.uid == uid).execute()
+	except:
+		print('Fehler: Die Bewertung konnte nicht in der Datenbank gespeichert werden.')
+
+def temporaere_saetze_loeschen():
+	try:
+		Satz.delete().where(Satz.tmp == True).execute()
+	except:
+		print('Fehler: Die temporären Sätze konnten nicht aus der Datenbank gelöscht werden.')
+
+def bewertung_loggen(uid):
+	try:
+		Benutzer.create( # Benutzer hat Satz bewertet
+		satz_uid = uid,
+		ip = request.get('REMOTE_ADDR'),
+		voted = jetzt()
+		)
+	except:
+		print("Fehler: Bewerung konnte nicht geloggt werden.")
 
 @route('/')
-def index():
-	satz = z.satz()
-	randb_satz = counter()
-	return template('default', satz=satz, randb_satz=randb_satz)
+def generator():
+	redirect('/' + neuen_satz_generieren())
 
-@route('/', method='POST')
-def vote():
-	if request.POST.get('pro','').strip():
-		voted = 'pro'
-	if request.POST.get('contra','').strip():
-		voted = 'contra'
-	if request.POST.get('permalink','').strip():
-		voted = 'permalink'
-	ip = request.get('REMOTE_ADDR')
-	
-	satz = request.POST.get('satz')
-	try:
-		satz = satz.encode('raw_unicode_escape').decode('utf-8')
-	except:
-		print("Fehler beim utf-8 encodieren von: " + satz)
-	
-	# Suche nach bereits vorhandenem Satz
-	con = sqlite3.connect('datenbank.db')
-	c = con.cursor()
-	sql = "SELECT id FROM satz WHERE satz LIKE '" + satz +"'"
-	c.execute(sql)
-	id_in_db = None
-	for dsatz in c: # ersten treffen auswählen
-		id_in_db = dsatz[0]
-		break
-	c.close()
-	con.close()
-	
-	if id_in_db and voted == 'permalink': # wenn Satz bereits in DB gibe Permalink
-		redirect("http://satzgenerator.net/" + str(id_in_db) + "?permalink=true")
-	elif id_in_db: # wenn Satz bereits in DB, bewerten
-		con = sqlite3.connect('datenbank.db')
-		c = con.cursor()
-		
-		# alte user daten löschen
-		sql = "DELETE FROM user WHERE zeitstempel < " + str(int(time.time()) - 60*60*24)
-		c.execute(sql)
-		con.commit()
-				
-		# Zeit suchen, wann den Satz zu letzt abgestimmt
-		sql = "SELECT zeitstempel FROM user WHERE satz_id = '" + str(id_in_db) + "' AND ip = '" + ip + "'"
-		c.execute(sql)
-		dsatz = c.fetchall()
-		try:
-			damals = dsatz[0][0]
-		except:
-			damals = 0
-		
-		differenz = int(time.time()) - damals
-		
-		if differenz < 60*60*24:
-			redirect("http://satzgenerator.net/" + str(id_in_db) + "?error=" + str(differenz))
-		
-		sql = "UPDATE satz SET " + voted + "=" + voted + " + 1 WHERE id = '" + str(id_in_db) + "'"
-		c.execute(sql)
-		con.commit()
-		new_id = id_in_db
-		
-		# Bewertung speichern
-		zeitstempel = int(time.time())
-		sql = "INSERT INTO user (satz_id, ip, zeitstempel) VALUES(" + str(new_id) + ", '" + ip + "', " + str(zeitstempel) + ")"
-		c.execute(sql)
-		con.commit()
-		
-		c.close()
-		con.close()
-		redirect("http://satzgenerator.net/" + str(new_id))
-	elif voted == 'permalink': # Satz für Permalink in DB speichern
-		zeitstempel = time.strftime("%Y-%m-%d %H:%M:%S")
-		con = sqlite3.connect('datenbank.db')
-		sql = "INSERT INTO satz (pro, contra, zeitstempel, satz) VALUES(0, 0, '"+ zeitstempel + "', '" + satz + "')"
-		c = con.cursor()
-		c.execute(sql)
-		new_id = c.lastrowid
-		con.commit()
-		c.close()
-		con.close()
-		redirect("http://satzgenerator.net/" + str(new_id) + "?permalink=true")
-	else: # Satz in DB speichern mit Bewertung
-		if voted == 'pro':
-			procon = '1, 0'
-		else:
-			procon = '0, 1'
-		
-		zeitstempel = time.strftime("%Y-%m-%d %H:%M:%S")
-		con = sqlite3.connect('datenbank.db')
-		sql = "INSERT INTO satz (pro, contra, zeitstempel, satz) VALUES(" + procon +", '"+ zeitstempel + "', '" + satz + "')"
-		c = con.cursor()
-		c.execute(sql)
-		new_id = c.lastrowid
-		con.commit()
-		
-		# Bewertung speichern
-		zeitstempel = int(time.time())
-		sql = "INSERT INTO user (satz_id, ip, zeitstempel) VALUES(" + str(new_id) + ", '" + ip + "', " + str(zeitstempel) + ")"
-		c.execute(sql)
-		con.commit()
-		
-		c.close()
-		con.close()
-		redirect("http://satzgenerator.net/" + str(new_id))
+@route('/<uid:re:[a-z]{5}>', method='GET')
+def satz_detailseite(uid):
+	satz = Satz.select().where(Satz.uid == uid).get()
+	return template('default', satz_uid=satz.uid, satz=satz.satz, positiv=satz.pro, negativ=satz.kontra)
 
-@route('/<sid:int>')
-def satz_db(sid):
-	# Permalink direkt anzeigen
-	permalink = False
-	permalink = request.query.permalink # ?permalink=true
-	
-	if permalink:
-		permalink = "<script>document.onload=show_permalink()</script>"
-	
-	# Fehlermeldung anzeigen
-	fehler = False
-	fehler = request.query.error # ?error=zeit_differenz
-	
-	if fehler:
-		fehler = "<script>document.onload=show_warning()</script>"
-	
-	con = sqlite3.connect('datenbank.db')
-	sql = "SELECT * FROM satz WHERE id =  '" + str(sid) +"'"
-	c = con.cursor()
-	c.execute(sql)
-	
-	for dsatz in c:
-		pos, neg, satz = dsatz[1], dsatz[2], dsatz[4]
-	c.close()
-	con.close()
-	return template('default', satz=satz, pos=pos, neg=neg, satz_id=sid, permalink=permalink, fehler=fehler)
+@route('/<uid:re:[a-z]{5}>', method='POST')
+def satz_bewerten(uid):
+	req = request.forms.text
+	if req == "pro":
+		satz_positiv_bewerten(uid)
+		bewertung_loggen(uid)
+		satz = Satz.select().where(Satz.uid == uid).get()
+		return str(satz.pro) + ',' + str(satz.kontra)
+	elif req == "kontra":
+		satz_negativ_bewerten(uid)
+		bewertung_loggen(uid)
+		satz = Satz.select().where(Satz.uid == uid).get()
+		return str(satz.pro) + ',' + str(satz.kontra)
+	if req == "permalink":
+		satz_permanent_speichern(uid)
 
-@route('/beste-bewertung')
-def beste():
-	con = sqlite3.connect('datenbank.db')
-	sql = "SELECT * FROM satz WHERE pro >= contra ORDER BY pro DESC, contra LIMIT 25"
-	c = con.cursor()
-	c.execute(sql)
-	satze = c.fetchall()
-	c.close()
-	con.close()
-	return template('stats', titel="Die Sätze mit den besten Bewertungen", satze=satze)
-
-
-@route('/schlechteste-bewertung')
-def schlechte():
-	con = sqlite3.connect('datenbank.db')
-	sql = "SELECT * FROM satz WHERE contra >= pro ORDER BY contra DESC, pro LIMIT 25"
-	c = con.cursor()
-	c.execute(sql)
-	satze = c.fetchall()
-	c.close()
-	con.close()
-	return template('stats', titel="Die Sätze mit den schlechtesten Bewertungen", satze=satze)
-
-@route('/meiste-bewertungen')
-def meiste():
-	con = sqlite3.connect('datenbank.db')
-	sql = "SELECT * FROM satz ORDER BY pro+contra DESC LIMIT 25"
-	c = con.cursor()
-	c.execute(sql)
-	satze = c.fetchall()
-	c.close()
-	con.close()
-	return template('stats', titel="Die Sätze mit den meisten Bewertungen", satze=satze)
-
-@route('/neuste-saetze')
-def meiste():
-	con = sqlite3.connect('datenbank.db')
-	sql = "SELECT * FROM satz ORDER BY zeitstempel DESC LIMIT 25"
-	c = con.cursor()
-	c.execute(sql)
-	satze = c.fetchall()
-	c.close()
-	con.close()
-	return template('stats', titel="Die neusten Sätze", satze=satze)
-
-@route('/assets/<filepath:path>')
+@route('/bootstrap/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root='./assets/')
+    return static_file(filepath, root='./bootstrap/')
 
 @error(404)
 def error404(error):
     return '<h1>Diese Seite existiert nicht.</h1>'
 
-#run(reloader=True, host='10.0.0.11', port=8080)
+# allow running from the command line
+if __name__ == '__main__':
+	run(reloader=True, debug=True, host='10.0.0.11', port=8080)
--- views/default.tpl
+++ views/default.tpl
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
 %try:
-	%satz_id
+	%satz_uid
     <title>Satzgenerator: {{get('satz', '')}}</title>
 %except:
 	<title>Satzgenerator</title>
@@ -13,7 +13,7 @@
     <meta name="keywords" content="Satz generieren, Sätze, Fakten, Satzgenerator, deutsch, Open Source, Python, absurd, lustig, bewerten, download" />
     <meta name="author" content="davidak">
     <meta name="robots" content="index,follow">
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
     <style>
       body {
       padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
@@ -37,18 +37,19 @@
       .btn-toolbar { margin-top: 20px; }
       }
     </style>
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
       </script>
     <![endif]-->
-    <!-- fav and touch icons -->
-    <link rel="shortcut icon" href="assets/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+    <!-- fav and touch icons
+    <link rel="shortcut icon" href="bootstrap/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="bootstrap/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="bootstrap/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="bootstrap/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="bootstrap/ico/apple-touch-icon-57-precomposed.png">
+    -->
 
   </head>
   <body>
@@ -84,18 +85,13 @@
     <div id="content" class="container-fluid">
       <h3>{{get('satz', 'Fehler beim generieren des Satzes.')}}</h3>
       
-      <form action="/" method="POST">
-      <input name='satz' type='hidden' value='{{get('satz', '')}}'>
+      <form id="bewerten" action="#" method="POST">
       <div class="btn-toolbar">
-      <button type="submit" name="pro" value='1' class="btn btn-success"><i class="icon-thumbs-up icon-white"></i><span class="hidden-phone"> Gefällt mir</span></button> <!--disabled-->
-      <button type="submit" name="contra" value='1' class="btn btn-danger"><i class="icon-thumbs-down icon-white"></i></button>
-%try:
-	%satz_id
-	<a class="btn btn-primary" href="#" onclick="show_permalink()">Teilen</a>
-%except:
-	<button type="submit" name="permalink" value='1' class="btn btn-primary">Teilen</button>
-%end
-      <a class="btn btn-inverse" href="/{{get('randb_satz', '')}}"><i class="icon-refresh icon-white"></i> Neuer Satz</a>
+      <button id="pos" type="submit" name="pro" class="btn btn-success"><i class="icon-thumbs-up icon-white"></i><span class="hidden-phone"> Gefällt mir</span></button> <!--disabled-->
+      <button id="neg" type="submit" name="kontra" class="btn btn-danger"><i class="icon-thumbs-down icon-white"></i></button>
+      <button id="per" type="submit" name="permalink" onclick="show_permalink()" class="btn btn-primary">Teilen</button>
+
+      <a class="btn btn-inverse" href="/"><i class="icon-refresh icon-white"></i> Neuer Satz</a>
       </div>
       </form>
 
@@ -104,32 +100,75 @@
        Du hast diesen Satz heute schon bewertet.
       </div>
 
-%try:
-%	ges = pos + neg
-%	pos_proz = float(pos)/ges * 100
-%	pos_proz = int(pos_proz)
-%	neg_proz = 100-pos_proz
-	<div class="progress">
-	<div class="bar bar-success" style="width: {{str(pos_proz)}}%;">{{str(pos)}}</div>
-	<div class="bar bar-danger" style="width: {{str(neg_proz)}}%;">{{str(neg)}}</div>
-	</div>
-%except:
-%	pass
-%#	print("Fehler: Keine Bewertungsdaten übergeben.")
-%end
+<div id="bewertung" class="progress"><!-- Bewertung anzeigen -->
+<div id="positiv_prozent_balken" class="bar bar-success" style="width: 0%;">1</div>
+<div id="negativ_prozent_balken" class="bar bar-danger" style="width: 0%;">1</div>
+</div>
 
-    <input class="input-block-level" id="permalink" type="text" value="http://satzgenerator.net/{{get('satz_id', 'satz_id')}}">
+    <input class="input-block-level" id="permalink" type="text" value="http://satzgenerator.net/{{get('satz_uid')}}">
 
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script> -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+
     <script>function show_permalink() {document.getElementById('permalink').style.display='block'; document.getElementById('permalink').select();}</script>
     <script>function show_warning() {document.getElementById('warnung').style.display='block';}</script>
     <script>function hide_warning() {document.getElementById('warnung').style.display='none';}</script>
     {{!get('permalink', '')}}
     {{!get('fehler', '')}}
-    
+
+<script type="text/javascript">
+// Bewertung abschicken
+$("#pos, #neg, #per").click(function() {
+    $.ajax({
+           type: "POST",
+           url: "#",
+           dataType: 'text',
+           data : { text: $(this).attr("name") },
+           success: function(data)
+           {
+              if (data)
+              {
+              var vars = data.split(",");
+              bewertung_anzeigen(vars[0], vars[1]);
+              }
+           }
+         });
+
+    event.preventDefault();
+});
+
+// Bewertung anzeigen
+positiv = {{get('positiv', '0')}};
+negativ = {{get('negativ', '0')}};
+
+if (positiv == 0 && negativ == 0)
+{
+  document.getElementById('bewertung').style.display='none';
+}
+else
+{
+  bewertung_anzeigen(positiv, negativ);
+}
+
+function bewertung_anzeigen(positiv, negativ)
+{
+    document.getElementById('bewertung').style.display='block';
+
+    var gesamt = parseInt(positiv) + parseInt(negativ);
+    positiv_prozent = parseInt(positiv / gesamt * 100);
+    negativ_prozent = parseInt(100 - positiv_prozent);
+
+    document.getElementById('positiv_prozent_balken').style.width = positiv_prozent + "%";
+    document.getElementById('positiv_prozent_balken').innerHTML = positiv;
+
+    document.getElementById('negativ_prozent_balken').style.width = negativ_prozent + "%";
+    document.getElementById('negativ_prozent_balken').innerHTML = negativ;
+}
+</script>
+
 <!-- Piwik --> 
 <script type="text/javascript">
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://davidak.de/stats/" : "http://davidak.de/stats/");
