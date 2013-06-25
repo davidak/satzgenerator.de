eb9e30a95b36ad0ebf2e335940f1871fa9e1b187
davidak <git@davidak.de>
2013-06-25 19:31:28 +0200

    * Fehler in cron.py behoben?

--- cron.py
+++ cron.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # jeden Tag 4 Uhr
+import site
+site.addsitedir('/home/davidak/websites/satzgenerator/lib/python2.7/site-packages')
+
 from satzgenerator import cron
 cron()
